7ec5dcbaada76c8f5804607f61e075293aea8638
luis-domingues <luisdomingues117@gmail.com>
2024-07-30 17:21:05 -0300

    feat: dashboard structure template

---
+++ public/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/dist/output.css">
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+</head>
+
+<body>
+    <nav class="bg-gray-100 text-gray-700 border border-gray-700 h-full w-16 fixed">
+        <div class="flex items-center justify-center px-3 pt-5">
+            <p class="text-xs font-bold">devboard</p>
+        </div>
+        <div class="flex flex-col space-y-6 mt-16">
+            <a href="#" class="flex items-center justify-center px-3 py-3 rounded-full bg-gray-300">
+                <i class="ph ph-layout text-2xl"></i>
+            </a>
+            <a href="#" class="flex items-center justify-center px-3 py-3 rounded-full hover:bg-gray-300">
+                <i class="ph ph-user-gear text-2xl"></i>
+            </a>
+            <a href="#" class="flex items-center justify-center px-3 py-3 rounded-full hover:bg-gray-300">
+                <i class="ph ph-pencil-simple-line text-2xl"></i>
+            </a>
+            <a href="#" class="flex items-center justify-center px-3 py-3 rounded-full hover:bg-gray-300">
+                <i class="ph ph-book-bookmark text-2xl"></i>
+            </a>
+        </div>
+    </nav>
+    <div class="flex flex-col items-center h-screen ml-5">
+        <div class="grid grid-cols-4 grid-rows-2 gap-3 py-5 border w-4/5">
+            <div class=" bg-blue-200 col-span-2 p-20">
+                //under developer
+            </div>
+            <div class=" bg-blue-200 col-span-2">
+                //
+            </div>
+            <div class=" bg-blue-200 ">
+                //under developer
+            </div>
+            <div class=" bg-blue-200 ">
+                //under developer
+            </div>
+            <div class=" bg-blue-200">
+                //under developer
+            </div>
+            <div class=" bg-blue-200">
+                //under developer
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
